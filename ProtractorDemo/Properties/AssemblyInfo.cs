5162ae36ff2c7cd3141b31ae72a1efaf9e460a10
kalyanii <kalyanii>
2017-07-25 05:21:04 +0530

    Add project files.

---
+++ ProtractorDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProtractorDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProtractorDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c4621f3-beb8-42cb-88b0-d41a2ad7718a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProtractorDemo/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Protractor;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace ProtractorDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetupMethod()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://www.angularjs.org");
+            ngDriver.FindElement(NgBy.Model("yourName")).SendKeys("Kalyani");
+            Assert.AreEqual("Hello Kalyani!", ngDriver.FindElement(NgBy.Binding("yourName")).Text);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Manage().Window.Maximize();
+            ngDriver.Navigate().GoToUrl("https://danielykpan.github.io/ng2-fan-menu/");
+            ngDriver.FindElement(By.ClassName("menu-btn")).Click();
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://www.way2automation.com/angularjs-protractor/banking");
+            
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
