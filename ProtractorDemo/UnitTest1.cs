5bd4fb30320a112cb135fc55c4b144b2d812152a
Kalyani Thosar <thosar.kalyani92@gmail.com>
2017-07-31 04:39:32 +0530

    changed

--- ProtractorDemo/UnitTest1.cs
+++ ProtractorDemo/UnitTest1.cs
@@ -3,6 +3,8 @@ using Protractor;
 using OpenQA.Selenium;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.UI;
 
 namespace ProtractorDemo
 {
@@ -10,11 +12,13 @@ namespace ProtractorDemo
     public class UnitTest1
     {
         private IWebDriver driver;
+        public WebDriverWait wait;
 
         [SetUp]
         public void SetupMethod()
         {
             driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
         }
 
         [Test]
@@ -39,8 +43,28 @@ namespace ProtractorDemo
         public void TestMethod3()
         {
             var ngDriver = new NgWebDriver(driver);
+            ngDriver.Manage().Window.Maximize();
             ngDriver.Navigate().GoToUrl("http://www.way2automation.com/angularjs-protractor/banking");
-            
+            wait.Until(ExpectedConditions.ElementExists(NgByBinding.XPath("//button[text()='Customer Login']")));
+            ngDriver.FindElement(NgByBinding.XPath("//button[text()='Customer Login']")).Click();
+            wait.Until(ExpectedConditions.ElementExists(NgBy.Model("custId")));
+            ReadOnlyCollection<NgWebElement> customers = ngDriver.FindElement(NgBy.Model("custId")).FindElements(NgBy.Repeater("cust in Customers"));
+            foreach (NgWebElement cust in customers)
+            {
+                if (cust.Text.Contains("Harry Potter"))
+                {
+                    cust.Click();
+                    break;
+                }
+                
+            }
+            wait.Until(ExpectedConditions.ElementExists(NgByBinding.XPath("//button[text()='Login']")));
+            ngDriver.FindElement(NgByBinding.XPath("//button[text()='Login']")).Click();
+            wait.Until(ExpectedConditions.ElementExists(NgByBinding.XPath("//button[contains(text(),'Deposit')]")));
+            ngDriver.FindElement(NgByBinding.XPath("//button[contains(text(),'Deposit')]")).Click();
+            ngDriver.FindElement(NgBy.Model("amount")).SendKeys("100");
+            ngDriver.FindElement(NgByBinding.XPath("//button[contains(@class,'btn btn-default')]")).Click();
+
 
         }
 
